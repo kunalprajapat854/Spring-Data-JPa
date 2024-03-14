9ee328d07785036e8d672b83e096390fbee8bdeb
kunalprajapat854 <virajprajapat854@gmail.com>
2024-03-14 22:08:44 +0530

    find all method

--- src/main/java/in/kunal/Service/BookService.java
+++ src/main/java/in/kunal/Service/BookService.java
@@ -121,7 +121,11 @@ public class BookService {
 		bookrepo.deleteid(id);
 	}	
 
-   
+   public List<Book> getallbooks(){
+	   List<Book> list =  Arrays.asList(1.3.5);
+	   Iterable<Book> itr = bookrepo.findAllById(list);
+	   itr.forEach(System.out::println);
+   }
 	
 	
 	
